9e0f203ed82905942d5cc35c0350ff402037b03c
Migue Cabrera <mcabrera@minutentag.com>
2018-10-14 17:32:01 -0300

    add utility file

---
+++ src/util/index.js
@@ -0,0 +1,24 @@
+export const ENDPOINT = "https://mcabreradev-truenorth-api.herokuapp.com";
+
+export const filterRestaurants = (state) => {
+
+    const {
+        restaurants,
+        name,
+        sortBy
+    } = state;
+
+    return restaurants.filter(restaurant => {
+        return restaurant.commercialName.toLowerCase().indexOf(name.toLowerCase()) >= 0 || restaurant.address.toLowerCase().indexOf(name.toLowerCase()) >= 0;
+    })
+    .sort((last, next) => {
+
+        if (sortBy === "") {
+            return last.commercialName - next.commercialName; // sort by name ASC
+        }
+
+        // next.rating - last.rating -> ASC
+        // last.rating - next.rating -> DESC
+        return sortBy === "highest_rated" ? next.rating - last.rating : last.rating - next.rating;
+    });
+}
