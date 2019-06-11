2e1920a131cd620232e66a61c1a47a5b566ee84f
Miguelangel Cabrera <miguel.angel@elliemae.com>
2019-06-10 23:49:09 -0300

    Updates

---
+++ precache-manifest.e14565b23a9cec13d02e4d7c0ff9a207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "118cf7de0205b36dbaef",
+    "url": "/truenorth-challenge/static/js/runtime~main.118cf7de.js"
+  },
+  {
+    "revision": "8f0b1a6c3ee7fe8e70a4",
+    "url": "/truenorth-challenge/static/js/main.8f0b1a6c.chunk.js"
+  },
+  {
+    "revision": "4f655b572df976eb4b2f",
+    "url": "/truenorth-challenge/static/js/1.4f655b57.chunk.js"
+  },
+  {
+    "revision": "8f0b1a6c3ee7fe8e70a4",
+    "url": "/truenorth-challenge/static/css/main.ce425d16.chunk.css"
+  },
+  {
+    "revision": "4f655b572df976eb4b2f",
+    "url": "/truenorth-challenge/static/css/1.b548c782.chunk.css"
+  },
+  {
+    "revision": "e7de49c491a12ab187d368de39defc88",
+    "url": "/truenorth-challenge/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/truenorth-client/precache-manifest.cc018c9a054052eb50200a785fb96438.js"
+  "/truenorth-challenge/precache-manifest.e14565b23a9cec13d02e4d7c0ff9a207.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/truenorth-client/index.html", {
+workbox.routing.registerNavigationRoute("/truenorth-challenge/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
